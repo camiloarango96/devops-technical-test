bc837425158b0772ef1b62e3886d4c92f75e003d
camiloarango <camiloarango96@gmail.com>
2022-10-23 11:44:14 -0500

    creo script para gregar record a r53

---
+++ scripts/createRecord.py
@@ -0,0 +1,25 @@
+from urllib import response
+import boto3 
+
+elb = boto3.client('elb')
+r53 = boto3.client('route53')
+lbs = elb.describe_load_balancers()
+lb_DNS = (lbs['LoadBalancerDescriptions'][0]['DNSName'])
+
+print(lb_DNS)
+
+response = r53.change_resource_record_sets(
+    HostedZoneId = 'Z0980157NFULAKVHAECQ',
+    ChangeBatch = {
+        'Comment': 'Creo el record para el load balancer',
+        "Changes": [{
+            "Action": "CREATE",
+                        "ResourceRecordSet": {
+                                    "Name": "flask.olimac.link",
+                                    "Type": "CNAME",
+                                    "TTL": 300,
+                                    "ResourceRecords": [{ "Value": lb_DNS}]
+                    }}]
+    }
+)
+
