08475b48d1f282b686281f02bfe7a9badeafb9ae
camiloarango <camiloarango96@gmail.com>
2022-10-23 23:51:19 -0500

    correccion de script db

--- scripts/getDBEndpoint.py
+++ scripts/getDBEndpoint.py
@@ -9,10 +9,13 @@ host = instances['DBInstances'][0]['Endpoint']['Address']
 
 name = instances['DBInstances'][0]['DBInstanceIdentifier']
 
-db_host_string = "DB_HOST={}".format(host)
+db_host_string = "DB_HOST={}\n".format(host)
 db_name_string = "DB_NAME={}".format(name)
 
+print(db_host_string)
+print(db_name_string)
+
 env_file = os.system('GITHUB_ENV')
-with open(env_file, "a") as myfile:
+with open(env_file, "w") as myfile:
     myfile.write(db_host_string)
     myfile.write(db_name_string)
