b4e8e8a575a9519a7877450940e3813c8603e03d
camiloarango <camiloarango96@gmail.com>
2022-10-23 22:45:46 -0500

    agrego iac para rds

---
+++ iac/rds.tf
@@ -0,0 +1,19 @@
+resource "aws_db_subnet_group" "db_subnet_group" {
+  name          = "db_subnet_group"
+  subnet_ids    = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id]
+
+  tags = {
+    Name = "Grupo de subredes para la db"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  allocated_storage     = 10
+  db_name               = var.db_name
+  engine                = "postgresql"
+  instance_class        = "db.t3.micro"
+  username              = var.db_user
+  password              = var.db_password
+
+  db_subnet_group_name  = aws_db_subnet_group.db_subnet_group.name
+}
