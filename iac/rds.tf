93f36800c49c87f2a3dc773f67001b85d2818ed9
camiloarango <camiloarango96@gmail.com>
2022-10-24 16:35:08 -0500

    rds en default vpc

--- iac/rds.tf
+++ iac/rds.tf
@@ -1,11 +1,11 @@
-resource "aws_db_subnet_group" "db_subnet_group" {
-  name          = "db_subnet_group"
-  subnet_ids    = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id, aws_subnet.public-us-east-1a.id, aws_subnet.public-us-east-1b.id]
+# resource "aws_db_subnet_group" "db_subnet_group" {
+#   name          = "db_subnet_group"
+#   subnet_ids    = [aws_subnet.private-us-east-1a.id, aws_subnet.private-us-east-1b.id, aws_subnet.public-us-east-1a.id, aws_subnet.public-us-east-1b.id]
 
-  tags = {
-    Name = "Grupo de subredes para la db"
-  }
-}
+#   tags = {
+#     Name = "Grupo de subredes para la db"
+#   }
+# }
 
 resource "aws_db_instance" "postgres" {
   allocated_storage     = 10
@@ -18,7 +18,7 @@ resource "aws_db_instance" "postgres" {
   port                  = 5432
   publicly_accessible   = true
 
-  db_subnet_group_name  = aws_db_subnet_group.db_subnet_group.name
+  
 }
 
 
