efcae92adc12aa666461cd353b1dead6790ba485
camiloarango <camiloarango96@gmail.com>
2022-10-25 18:49:20 -0500

    creacion de vpc y sg para rds

--- iac/vpc.tf
+++ iac/vpc.tf
@@ -8,3 +8,33 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+resource "aws_security_group" "rds_sg" {
+
+  ingress = {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
+resource "aws_vpc" "rds" {
+  cidr_block = "10.0.0.0/16"
+
+  enable_dns_support = true
+  enable_dns_hostnames = true
+  default_security_group_id = aws_security_group.rds_sg.id
+
+  tags = {
+    Name = "main"
+  }
+}
