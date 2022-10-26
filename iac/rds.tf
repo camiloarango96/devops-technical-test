2676d3a2e8eb472bdf6b6ec5355f642ed7403e27
camiloarango <camiloarango96@gmail.com>
2022-10-25 19:22:24 -0500

    creacion de vpc y sg para rds

--- iac/rds.tf
+++ iac/rds.tf
@@ -7,6 +7,24 @@
 #   }
 # }
 
+resource "aws_security_group" "rds_sg" {
+  name = "grupo de seguridad para db postgres"
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+}
+
 resource "aws_db_instance" "postgres" {
   allocated_storage     = 10
   #db_name               = var.db_name
--- iac/vpc.tf
+++ iac/vpc.tf
@@ -9,37 +9,4 @@ resource "aws_vpc" "main" {
   }
 }
 
-resource "aws_security_group" "rds_sg" {
 
-  ingress {
-    from_port   = 5432
-    to_port     = 5432
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  
-}
-
-resource "aws_vpc" "rds" {
-  cidr_block = "10.0.0.0/16"
-
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "rds"
-  }
-}
-
-resource "aws_subnet" "rds-subnet" {
-  vpc_id            = aws_vpc.rds.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-east-1a"
-}
