a2ebbbe2aa9c5c62edd8dca593e73422030ad1de
camiloarango <camiloarango96@gmail.com>
2022-10-25 20:05:41 -0500

    creacion de vpc y sg para rds

--- iac/vpc.tf
+++ iac/vpc.tf
@@ -9,27 +9,3 @@ resource "aws_vpc" "main" {
   }
 }
 
-
-resource "aws_vpc" "rds" {
-  cidr_block = "10.0.0.0/16"
-
-  enable_dns_support = true
-  enable_dns_hostnames = true
-
-  tags = {
-    Name = "rds"
-  }
-}
-
-resource "aws_subnet" "rds-subnet-a" {
-  vpc_id            = aws_vpc.rds.id
-  cidr_block        = "10.0.0.0/19"
-  availability_zone = "us-east-1a"
-}
-
-resource "aws_subnet" "rds-subnet-b" {
-  vpc_id            = aws_vpc.rds.id
-  cidr_block        = "10.0.32.0/19"
-  availability_zone = "us-east-1b"
-}
-
